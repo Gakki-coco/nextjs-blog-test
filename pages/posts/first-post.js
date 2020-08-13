1ac5c7d2c8f720d97ead40a9c56fd7c628adebb8
Dolores <alyciahathaways@gmail.com>
2020-08-13 15:47:15 +0800

    Link 快速导航

---
+++ pages/posts/first-post.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Link from 'next/link'
+
+export default function X() {
+    return (
+        <div>
+            First Post 回到首页
+            <Link href={"/"}><a>点击这里</a></Link>
+        </div>
+    )
+}
