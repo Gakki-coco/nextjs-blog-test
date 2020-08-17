9379f2d0710c6f0507f303c86fd9a9f9d12b6d21
Dolores <alyciahathaways@gmail.com>
2020-08-17 12:52:03 +0800

    完成 POSTS API

--- pages/api/v1/posts.tsx
+++ pages/api/v1/posts.tsx
@@ -1,9 +1,11 @@
 import {NextApiHandler} from 'next'
+import {getPosts} from 'lib/posts'
 
-const Posts: NextApiHandler = (req, res) => {
+const Posts: NextApiHandler = async (req, res) => {
+    const posts = await getPosts()
     res.statusCode = 200
     res.setHeader('Content-Type', 'application/json')
-    res.write(JSON.stringify({name: 'Gakki'}))
+    res.write(JSON.stringify(posts))
     res.end()
 }
 
