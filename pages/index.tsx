9c1c30fd439cfe3543dfe62772ac3427afc36c70
Dolores <alyciahathaways@gmail.com>
2020-08-19 21:31:32 +0800

    第三种渲染方式：SSR

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,38 @@
-import React from 'react'
-import png from 'assets/images/1.jpg'
+import {GetServerSideProps, NextPage} from 'next'
+import {UAParser} from 'ua-parser-js'
+import {useEffect, useState} from 'react'
 
-export default function Index() {
+type Props = {
+    browser: {
+        name: string
+        version: string
+        major: string
+    }
+}
+const Index: NextPage<Props> = (props) => {
+    const {browser} = props
+    const [width, setWidth] = useState(0)
+    useEffect(() => {
+        const w = document.documentElement.clientWidth
+        setWidth(w)
+    })
     return (
         <div>
-            <h1>标题</h1>
-            <p>段落</p>
-            <img src={png} alt={"png"}/>
+            <p>我是服务端渲染的</p>
+            <h1>你的浏览器是 {browser.name}</h1>
+            <p>我是客户端渲染的</p>
+            <h1>你的浏览器窗口大小是 {width} 像素</h1>
         </div>
     )
+}
+export default Index
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+    const ua = context.req.headers['user-agent']
+    const result = new UAParser(ua).getResult()
+    return {
+        props: {
+            browser: result.browser
+        }
+    }
 }
