c8d65fa4ece72f164c7db93502a883744894c672
Dolores <alyciahathaways@gmail.com>
2020-08-19 10:17:44 +0800

    重构 usePosts 自定义的 hooks

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,29 +1,9 @@
 import {NextPage} from 'next'
-import React, {useEffect, useState} from 'react'
-import axios from 'axios'
-
-type Post = {
-    id: string;
-    date: string;
-    title: string;
-}
+import {usePosts} from 'hooks/usePosts'
 
 const PostsIndex: NextPage = () => {
-    const [posts, setPosts] = useState<Post[]>([])
-    const [isLoading, setIsLoading] = useState(false)
-    const [isEmpty, setIsEmpty] = useState(false)
-    useEffect(() => {
-        setIsLoading(true)
-        axios.get('/api/v1/posts').then(response => {
-            setPosts(response.data)
-            setIsLoading(false)
-            if (response.data.length === 0) {
-                setIsEmpty(true)
-            }
-        }, () => {
-            setIsLoading(false)
-        })
-    }, [])
+    const {isLoading, isEmpty, posts} = usePosts()
+
     return (
         <div>
             <h1>文章列表</h1>
