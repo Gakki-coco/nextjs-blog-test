ee010123e57ff2d3d6cb9b15f1840df8ac5f3071
Dolores <alyciahathaways@gmail.com>
2020-08-30 11:42:49 +0800

    使用 .evn.local 设置 session 密钥

--- lib/withSession.tsx
+++ lib/withSession.tsx
@@ -3,8 +3,7 @@ import {withIronSession} from 'next-iron-session'
 
 export default function withSession(handler: NextApiHandler) {
     return withIronSession(handler, {
-        // password: process.env.SECRET_COOKIE_PASSWORD
-        password: '11892878465950019118928784659500191189287846595001911892878465950019',
+        password: process.env.SECRET,
         cookieName: 'blog',
         cookieOptions: {
             secure: false
